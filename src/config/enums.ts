189a02195e78c06fbe96ced7c6066d0b4c7198ea
Subham-112 <subhambusiness286@gmail.com>
2026-03-12 19:25:38 +0530

    fix: correct spelling of StudentStatus enum and remove unused student model

--- src/config/enums.ts
+++ src/config/enums.ts
@@ -1,4 +1,4 @@
-export enum StudentStaus {
+export enum StudentStatus {
     ACTIVE = "active",
     INACTIVE = "inactive",
     SUSPENDED = "suspended",
--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,12 +1,6 @@
 import mongoose from "mongoose";
 import { config } from "./config";
 import { logColors, logger } from "../utils/logger.util";
-// Import all models to register them with Mongoose
-import TenantModel from "../models/tenant.model";
-import BranchModel from "../models/branch.model";
-import OwnerModel from "../models/owner.model";
-import StudentModel from "../models/student.model";
-import TeacherModel from "../models/teacher.model";
 
 const isSrvUrl = config.db.isSrv;
 const url = isSrvUrl ? `${config.db.url}/${config.db.name}` : `${config.db.url}/${config.db.name}?ssl=true&replicaSet=atlas-i0izia-shard-0&authSource=admin&appName=Cluster0`
